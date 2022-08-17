f82dffb329b043430002c1efd0357480e404d972
Mihail Parushkin <mihail.parushkin@yandex.ru>
2022-08-17 10:27:29 +0700

    add silent mode

--- src/stattests.py
+++ src/stattests.py
@@ -3,11 +3,14 @@ import numpy as np
 import pandas as pd
 import scipy.stats
 
+# НЕСКОЛЬКО ГРУПП, ПРОВЕРКА ГИПОТЕЗЫ, ЧТО ВСЕ СРЕДНИЕ РАВНЫ
+
 
 def f_test(
     size: Union[int, list, np.ndarray, pd.Series],
     means: Union[list, np.ndarray, pd.Series],
     stds: Union[list, np.ndarray, pd.Series],
+    silent: bool = True,
 ) -> None:
     """
     Проверка гипотезы, что все средние равны (исслед. факторы не оказ. никакого влияния и полученные различия случайны).
@@ -40,15 +43,16 @@ def f_test(
     F01 = scipy.stats.f.ppf(q=1 - 0.01, dfn=vbg, dfd=vwg)
     F05 = scipy.stats.f.ppf(q=1 - 0.05, dfn=vbg, dfd=vwg)
 
-    print(f"Межгрупповая дисперсия: MSbg = {MSbg:.3f}")
-    print(f"Внутригрупповая дисперсия: MSwg = {MSwg:.3f}\n")
-    print(f"F = MSbg / MSwg = {F:.3f}\n")
-    print(f"P = {p:.3f}\n")
-    print(f"Межгрупповое число степеней свободы: vbg = {vbg}")
-    print(f"Внутригрупповое число степеней свободы: vwg = {vwg}\n")
-    print(
-        f"Критический значение для заданного числа степеней сввободы и уровней значимости 0.01 и 0.05: \n\t F01 = {F01:.3f} \n\t F05 = {F05:.3f}\n"
-    )
+    if not silent:
+        print(f"Межгрупповая дисперсия: MSbg = {MSbg:.3f}")
+        print(f"Внутригрупповая дисперсия: MSwg = {MSwg:.3f}\n")
+        print(f"F = MSbg / MSwg = {F:.3f}\n")
+        print(f"P = {p:.3f}\n")
+        print(f"Межгрупповое число степеней свободы: vbg = {vbg}")
+        print(f"Внутригрупповое число степеней свободы: vwg = {vwg}\n")
+        print(
+            f"Критический значение для заданного числа степеней сввободы и уровней значимости 0.01 и 0.05: \n\t F01 = {F01:.3f} \n\t F05 = {F05:.3f}\n"
+        )
 
     if F > F01:
         print(
